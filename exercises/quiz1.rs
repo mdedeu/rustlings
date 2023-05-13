9eb6f4337df0baae91e38e742e4df1a14f045c35
marquitos.eth <mdedeu@itba.edu.ar>
2023-05-13 18:44:38 -0300

    feat: if and primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
+const APPLE_NORMAL_PRICE:i32 = 2;
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(amount:i32)->i32 {
+    if amount > 40{
+        return amount;
+    }else {
+        return amount*APPLE_NORMAL_PRICE;
+    }
+}
 
 // Don't modify this function!
 #[test]
